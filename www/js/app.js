e4da833833f0030d49fcc83d4351dea105715998
Auberon Lopez <auberonlopez@gmail.com>
2016-01-20 18:02:05 -0800

    fixed merging wackiness

--- www/js/app.js
+++ www/js/app.js
@@ -79,10 +79,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ui.bootstrap'])
         }
       }
     })
-<<<<<<< HEAD
-=======
-
->>>>>>> upstream/master
+  
     .state('app.playlists', {
       url: '/playlists',
       views: {
@@ -131,10 +128,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ui.bootstrap'])
         }
       }
   });
-<<<<<<< HEAD
-  
-=======
->>>>>>> upstream/master
 
 // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/playlists');
